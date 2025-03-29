1a18058005b02d28182186a75b77dc4c2ee3955d
Jonathan <jonathan.nafziger@packback.co>
2025-03-28 22:44:54 -0400

    Revert "devbox commit"
    This reverts commit 74cf633b72a2066e0a043f94c7586c93cd577e2a.

---
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,14 @@
+return {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function() 
+        vim.opt.termguicolors = true
+        local bufferline = require('bufferline')
+        bufferline.setup {
+            options = {
+               separator_style = "slant"
+            }
+        }
+    end
+}
