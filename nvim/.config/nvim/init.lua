0cd9656b8f620fc454f1dfa8c3420ccbcd146c88
Jonathan <jonathan.nafziger@packback.co>
2025-03-30 08:35:51 -0400

    add lualine

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,14 +3,15 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
 vim.cmd("set pastetoggle=<F10>")
 vim.cmd("set hidden")
-
+vim.cmd("set number")
+vim.cmd("set signcolumn=number")
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-vim.keymap.set('n', '<C-h', ":bnext<cr>", { desc = 'Next buffer' })
-vim.keymap.set('n', '<C-l>', ":bprevious<cr>", { desc = 'Previous buffer' })
+vim.keymap.set('n', '<A-l>', ":bnext<cr>", { desc = 'Next buffer' })
+vim.keymap.set('n', '<A-h>', ":bprevious<cr>", { desc = 'Previous buffer' })
 vim.keymap.set('n', '<C-n>', ":Neotree filesystem filesystem reveal right<CR>", { desc = 'Open Neotree'})
 
 require("config.lazy")
--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -2,12 +2,15 @@ return {
     'akinsho/bufferline.nvim',
     version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function() 
+    config = function()
         vim.opt.termguicolors = true
         local bufferline = require('bufferline')
         bufferline.setup {
             options = {
-               separator_style = "slant"
+               separator_style = "slant",
+               numbers = function(opts)
+                   return opts.id
+               end,
             }
         }
     end
