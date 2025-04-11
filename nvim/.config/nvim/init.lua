90486caa392cc8fbe8802759fac52c285317a470
Jonathan <jonathan.nafziger@packback.co>
2025-04-11 07:59:29 -0400

    vim-sleuth for indentation

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,6 @@
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
-vim.cmd("set pastetoggle=<F10>")
 vim.cmd("set hidden")
 vim.cmd("set number")
 vim.cmd("set signcolumn=number")
